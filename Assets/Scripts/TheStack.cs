96f77f93de20b17eb7f198cadd6fce265f0c76ab
alexr101 <alex_r_101@hotmail.com>
2018-01-30 00:31:52 -0500

    add basic stack logic and place camera in ortographic mode

---
+++ Assets/Scripts/TheStack.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheStack : MonoBehaviour {
+
+	private GameObject[] theStack;
+	private int stackIndex;
+	private int scoreIndex = 0;
+
+	void Start () {
+		theStack = new GameObject[transform.childCount];
+
+		for (var i = 0; i < transform.childCount; i++) 
+		{
+			theStack [i] = transform.GetChild (i).gameObject;
+		}
+
+		stackIndex = transform.childCount - 1;
+
+
+	}
+
+	void Update () {
+		if( Input.GetKey("down") )
+		{
+			SpawnTile ();
+			scoreIndex++;
+		}
+	}
+
+	private void SpawnTile(){
+
+		theStack [stackIndex].transform.localPosition = new Vector3 (0, scoreIndex, 0);
+
+		stackIndex--;
+		if (stackIndex < 0)
+			stackIndex = transform.childCount-1;
+		
+
+
+	}
+
+	private void PlaceTile(){
+
+
+	}
+}
